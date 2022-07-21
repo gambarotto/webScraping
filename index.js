fe7b1ef35e76b76ab57a773ca5cb7e047b5a8573
gambarotto <dgcdeveloper@gmail.com>
2022-07-21 14:02:04 -0300

    fixed issue #1

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+import { existsSync, mkdirSync } from 'fs';
+import { dirname } from 'path';
 import { getAllPages } from './crawlerData.js';
+import { unifyFiles } from './unifyFiles.js';
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'y', 'z']
 
-getAllPages(0,alphabet.length, alphabet);
+const { pathname: currentFile } = new URL(import.meta.url);
+const cwd = dirname(currentFile);
+
+const tmp = `${cwd}/data-tmp`;
+const tmp2 = `${cwd}/data-tmp2`;
+
+if (!existsSync(tmp)){
+    mkdirSync(tmp);
+}
+if (!existsSync(tmp2)){
+    mkdirSync(tmp2);
+}
+const start = async () => {
+  await getAllPages(0,alphabet.length, alphabet);
+  await unifyFiles(cwd);
+}
+start();
--- unifyFiles.js
+++ unifyFiles.js
@@ -1,48 +1,76 @@
 import { readdir } from 'fs/promises';
-import { createReadStream, createWriteStream } from 'fs';
-import { dirname, join } from 'path';
+import { createReadStream, createWriteStream, appendFileSync, unlinkSync, rmSync } from 'fs';
+import { join } from 'path';
 import { promisify } from 'util';
-import { pipeline, Transform, Writable } from 'stream';
+import { pipeline, Transform } from 'stream';
 import StreamConcat from 'stream-concat';
 
-// transformando callbacks em promises
-const pipelineAsync = promisify(pipeline);
-
-const { pathname: currentFile } = new URL(import.meta.url);
-const cwd = dirname(currentFile);
-const filesDir = `${cwd}/data-tmp`;
-const output = './data/db-plants.json';
-const filesDirEach = `${cwd}/data-tmp2`;
-
 // setInterval(() => process.stdout.write('.'), 10).unref();
+// cwd = Current work directory
+export const unifyFiles = async (cwd) => {
 
-const files = await readdir(filesDir)
+  // transformando callbacks em promises
+  const pipelineAsync = promisify(pipeline);
 
-const streams = files.map(file => createReadStream(join(filesDirEach, file)));
-const combinedStreams = new StreamConcat(streams);
+  const filesDir = `${cwd}/data-tmp`;
+  const output = `${cwd}/data/db-plants.txt`;
+  const filesDirEach = `${cwd}/data-tmp2`;
 
-//const combinedStreams = createReadStream(join(filesDirEach, files[0]));
+  const files = await readdir(filesDir)
 
-async function each(){
-  try {
-    for await (const file of files){
-      
-      const read = createReadStream(join(filesDir, file));
-      const write = createWriteStream(join(filesDirEach, file))
+  const streams = files.map(file => createReadStream(join(filesDirEach, file)));
+  const combinedStreams = new StreamConcat(streams);
 
-      read.on('data', function(chunk) {
-        write.write(`"-,${Math.floor(Math.random()* 1000)}": ${chunk}`)
-      })
-    };
-  } catch (error) {
-    console.log(error);
+  //const combinedStreams = createReadStream(join(filesDirEach, files[0]));
+
+  async function each(){
+    try {
+      for await (const file of files){
+        
+        const read = createReadStream(join(filesDir, file));
+        const write = createWriteStream(join(filesDirEach, file))
+
+        read.on('data', function(chunk) {
+          write.write(`"-,${Math.floor(Math.random()* 1000)}": ${chunk}`)
+        })
+      };
+    } catch (error) {
+      console.log('erro', error);
+    }
   }
+  await each();
+
+  const finalStream = createWriteStream(output);
+
+  await pipelineAsync(
+    combinedStreams,
+    finalStream
+  )
+  
+  const finalRead = createReadStream(join(`${cwd}/data`,'db-plants.txt'));
+  const transformStream = new Transform({
+    transform(chunk, encoding, cb){
+      this.counter = this.counter ?? 0;
+      const data = chunk.toString();
+      const firstFormatted = data.replace(/"-,/g, `,"`);
+
+      if(this.counter === 0) {
+        const secondFormatted = firstFormatted.replace(/,/, '{');
+        this.counter += 1;
+        return cb(null, secondFormatted)
+      }
+
+      return cb(null,firstFormatted);
+    }
+  });
+  const finalWrite = createWriteStream(join(`${cwd}/data`,'db-plants.json'));
+  await pipelineAsync(
+    finalRead,
+    transformStream,
+    finalWrite
+  );
+  appendFileSync(join(`${cwd}/data`,'db-plants.json'), '}');
+  unlinkSync(join(`${cwd}/data`,'db-plants.txt'));
+  // rmSync(join(`${cwd}/data-tmp`),{force: true, recursive: true});
+  // rmSync(join(`${cwd}/data-tmp2`),{force: true, recursive: true});
 }
-await each();
-
-const finalStream = createWriteStream(output);
-
-await pipelineAsync(
-  combinedStreams,
-  finalStream
-)
